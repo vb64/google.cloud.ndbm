43658675eedc872af5433ca820f90a788dd31dbe
Vitaly Bogomolov <mail@vitaly-bogomolov.ru>
2023-02-21 09:36:48 +0400

    step

--- google_cloud_ndbm/flask.py
+++ google_cloud_ndbm/flask.py
@@ -4,7 +4,7 @@ from google.cloud import ndb
 
 
 def ndb_wsgi_middleware(wsgi_app, client):
-    """Flask middleware with ndb.Client context. """
+    """Flask middleware with ndb.Client context."""
     def middleware(environ, start_response):
         with client.context():
             return wsgi_app(environ, start_response)
@@ -13,7 +13,7 @@ def ndb_wsgi_middleware(wsgi_app, client):
 
 
 def bind(app, gae_project):
-    """Wrap the Flask app in middleware. """
+    """Wrap the Flask app in middleware."""
     # https://stackoverflow.com/questions/43628002/google-vision-api-project-not-passed
     os.environ["GCLOUD_PROJECT"] = gae_project
     app.wsgi_app = ndb_wsgi_middleware(app.wsgi_app, ndb.Client())
---
+++ tests/test/test_flask.py
@@ -0,0 +1,25 @@
+"""Test flask.py module.
+
+make test T=test_flask.py
+"""
+import os
+from . import TestBase
+
+
+class TestsFlask(TestBase):
+    """Module flask.py."""
+
+    def setUp(self):
+        """Set up test data."""
+        super(TestBase, self).setUp()
+        from flask import Flask
+
+        self.app = Flask(__name__)
+
+    def test_bind(self):
+        """Function bind."""
+        from google_cloud_ndbm.flask import bind
+
+        assert "GCLOUD_PROJECT" not in os.environ
+        assert bind(self.app, "xxx") is None
+        assert os.environ["GCLOUD_PROJECT"] == "xxx"
